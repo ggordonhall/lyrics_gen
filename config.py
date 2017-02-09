68ebe2169710ec6bae7f7d097800d6a3565a2bd7
Gabriel Gordon-Hall <GabrielGordon-Hall@c051198.wlan.net.ed.ac.uk>
2017-02-09 17:11:13 +0000

    Updated RDS endpoint

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://lyrical:$pitfire1@lyrical-generator.cmbz3vsmcqpp.us-west-2.rds.amazonaws.com:3306/lyricaldb'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'this-is-a-key'
+SECRET_KEY = 'AE9E57704F107C6757BF662981'
